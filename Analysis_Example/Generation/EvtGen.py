2a783c2408954c4532b374c547dfcd034c602615
david <you@example.com>
2020-09-29 14:46:56 -0600

    Example to crear mc sample

---
+++ Analysis_Example/Generation/EvtGen.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+########################################################
+#
+#
+#  Generate 1000 generic BBbar events using EvtGen
+#
+#
+########################################################
+
+import os
+import sys
+import basf2 as b2
+import mdst as mdst
+import generators as ge
+import simulation as si
+import reconstruction as re
+import modularAnalysis as ma
+
+# interpret the first input argument as decay file to facilitate testing of dec-files
+dec_file = None
+final_state = 'mixed'
+if len(sys.argv) > 1:
+    dec_file = os.path.abspath(sys.argv[1])
+    final_state = 'signal'
+    print("using following decay file: "+ dec_file)
+
+# main path
+my_path = b2.create_path()
+
+# event info setter
+my_path.add_module("EventInfoSetter", expList=0, runList=1, evtNumList=10000)
+
+# EvtGen
+ge.add_evtgen_generator(path=my_path, finalstate=final_state, signaldecfile=dec_file)
+
+# Simulation
+si.add_simulation(path=my_path)
+
+# Reconstruction
+re.add_reconstruction(path=my_path)
+
+# dump in MDST format
+mdst.add_mdst_output(path=my_path, mc=True, filename='OutputFiles/evtgen_upsilon4s.root')
+
+#run
+my_path.add_module("Progress")
+#main.add_module("PrintMCParticles", logLevel=basf2.LogLevel.DEBUG, onlyPrimaries=False)
+
+# generation events
+b2.process(my_path)
+
+# show call statistics
+print(b2.statistics)
